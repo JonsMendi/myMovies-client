0f9d08c320816441a774c998fe999f4d6415e3e8
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-01-24 23:37:36 +0100

    Created main-view.jsx with MainView component, contructor() the User state and render() the MovieCard.
    If applied, this commit will have created the main-view.jsx with MainView component that contruct the User state and render's the MovieCard.

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
+
+export class MainView extends React.Component {
+
+    constructor() {
+      super();
+      this.state = {
+        movies: [
+            { _id: 1, Title: 'Inception', Description: 'A lot of people sleeping all the time!', ImagePath: '...'},
+            { _id: 2, Title: 'The Shawshank Redemption', Description: 'A guy escapes the prison and live la vida loca!', ImagePath: '..'},
+            { _id: 3, Title: 'Gladiator', Description: 'The main character dies, don\'t watch!', ImagePath: '..'}
+        ]
+      }
+    }
+
+    setSelectedMovie(newSelectedMovie) {
+      this.setState({
+        selectedMovie: newSelectedMovie
+      });
+    }
+
+    render() {
+      const { movies, selectedMovie } = this.state;
+
+      if (movies.length === 0) return <div className="main-view">The list is empty brother!</div>;
+
+      return (
+        <div className = 'main-view'>
+          {selectedMovie
+        ? <MovieView movieData={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+        : movies.map(movie => (
+          <MovieCard key={movie._id} movieData={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
+        ))
+      }
+        </div>
+      );
+    }
+}
