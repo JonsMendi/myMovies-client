7925e5dc02c69aaaece713de8927a7749a839fca
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-01-25 12:03:40 +0100

    Added new fotos in movies objects.
    If applied, this commit will have added new images in each object of movies arrays.
    In this case was used a require built-in function to import the pictures. This process was possible because the pictures folder (img) is inside of src folder. Even though to keep the img folder in the public folder should be the future best practice, the require function looked the better for the moment necessities.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
+
 export class MainView extends React.Component {
 
     constructor() {
       super();
       this.state = {
         movies: [
-            { _id: 1, Title: 'Inception', Description: 'A lot of people sleeping all the time!', ImagePath: '...'},
-            { _id: 2, Title: 'The Shawshank Redemption', Description: 'A guy escapes the prison and live la vida loca!', ImagePath: '..'},
-            { _id: 3, Title: 'Gladiator', Description: 'The main character dies, don\'t watch!', ImagePath: '..'}
+            { _id: 1, Title: 'Inception', Description: 'A lot of people sleeping all the time!', ImagePath: require('../../img/inception.jpeg')},
+            { _id: 2, Title: 'The Shawshank Redemption', Description: 'A guy escapes the prison and live la vida loca!', ImagePath: require('../../img/tsr.jpeg')},
+            { _id: 3, Title: 'Gladiator', Description: 'The main character dies, don\'t watch!', ImagePath: require('../../img/gladiator.png')}
         ]
       }
     }
