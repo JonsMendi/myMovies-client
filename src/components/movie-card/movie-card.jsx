e6d1cd30aafa64d7e2bb9bdffc38671bf4e854ad
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-01-27 11:17:19 +0100

    Updated {movie.Data.Title} with <h2>
    If applied, this commit will have updated {movie.Data.Title} with <h2>.

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 export class MovieCard extends React.Component {
   render() {
     const { movieData, onMovieClick } = this.props;
-    return <div className="movie-card" onClick={() => { onMovieClick(movieData); }}>{movieData.Title}</div>;
+    return <div className="movie-card" onClick={() => { onMovieClick(movieData); }}>
+        <h2>{movieData.Title}</h2>
+        
+      </div>;
   }
 }
