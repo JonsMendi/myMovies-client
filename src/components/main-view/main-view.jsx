a8c495213e2834c1cc927965a9134b18128462ea
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-01-27 11:21:21 +0100

    Updated with new import axios library to fetch the online API.
    If applied, this commit will have import axios library to fetch the online API in the Class Component Lifecycle componentDidMount().
    In this case the movies objects in the array on the constructor() state was removed, leaving it with an empty array that will contain the fetch API.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
+import axios from 'axios';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
-
 export class MainView extends React.Component {
-
+    // constructor() starts before then render.
+    // constructor() is the method that React uses to actually create the component in-memory.
+    // super() it will call the parent React.Component’s constructor, which will give your class the actual React component’s features.
     constructor() {
       super();
       this.state = {
-        movies: [
-            { _id: 1, Title: 'Inception', Description: 'A lot of people sleeping all the time!', ImagePath: require('../../img/inception.jpeg')},
-            { _id: 2, Title: 'The Shawshank Redemption', Description: 'A guy escapes the prison and live la vida loca!', ImagePath: require('../../img/tsr.jpeg')},
-            { _id: 3, Title: 'Gladiator', Description: 'The main character dies, don\'t watch!', ImagePath: require('../../img/gladiator.png')}
-        ]
+        movies: [],
+        selectedMovie: null
       }
     }
 
@@ -22,10 +21,11 @@ export class MainView extends React.Component {
       });
     }
 
+    // render() renders the code into the virtualDOM 
     render() {
       const { movies, selectedMovie } = this.state;
 
-      if (movies.length === 0) return <div className="main-view">The list is empty brother!</div>;
+      if (movies.length === 0) return <div className="main-view" />;
 
       return (
         <div className = 'main-view'>
@@ -33,9 +33,23 @@ export class MainView extends React.Component {
         ? <MovieView movieData={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
         : movies.map(movie => (
           <MovieCard key={movie._id} movieData={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
-        ))
-      }
+            ))
+          }
         </div>
       );
+    }
+
+    componentDidMount(){
+      // code executed right after the component is added to the DOM.
+      // is a good place to add code for performing async tasks such as making ajax requests or adding event listeners(ex: fetch the list of movies from your database).
+      axios.get('https://mymovies-api-jbm.herokuapp.com/movies')
+        .then(response => {
+          this.setState({
+            movies: response.data
+          });
+        })
+        .catch(error => {
+          console.log(error);
+        });
     }
 }
