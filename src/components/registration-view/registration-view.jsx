c830688aa45a4afa57fdc34ff5ec40d9e0714ca0
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-01-28 11:29:37 +0100

    Updated with function RegistrationView that defines the basic components.
    If applied, this commit will have updated the RegistrationView with a function Component that contains the basic components.
    Using Hooks was defined the const that will set a state.
    Was added as well the propTypes.

---
+++ src/components/registration-view/registration-view.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import propTypes from 'prop-types';
+import './registration-views.scss';
+
+export function RegistrationView(props) {
+    const [ username, setUsername ] = useState('');
+    const [ password, setPassword ] = useState('');
+    const [ email, setEmail ] = useState('');
+    const [ birth, setBirth ] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        props.onRegister(username, password, email, birth)
+    };
+
+    return (
+        <div className='registration-view'>
+            <div>
+                <label>
+                    <h3>Username:</h3> <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+                </label>
+            </div>
+            <div>
+                <label>
+                    <h3>Password:</h3> <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+                </label>
+            </div>
+            <div>
+                <label>
+                    <h3>Username:</h3> <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+                </label>
+            </div>
+            <div>
+                <label>
+                    <h3>Username:</h3> <input type="text" value={birth} onChange={e => setBirth(e.target.value)} />
+                </label>
+            </div>
+            <div>
+                <button type="submit" onClick={handleSubmit}>Register</button>
+            </div>
+        </div>
+    )
+}
+// propTypes - Give warnings in browser/console if data does not match with the required.
+RegistrationView.propTypes = {
+    onRegister: propTypes.func//temporarily not required 
+}
