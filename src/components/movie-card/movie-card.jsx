eff0342b86d22c8d3fec596b021efdaae523f70b
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-02-01 16:01:08 +0100

    Wrapped the rendered information into <Card>.
    If applied, this commit will have wrapped the rendered information into <Card>.
    The "onClick" function was added to the general card.
    Was added a new <Card.Img> that contains the movie picture.

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import  Button  from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import './movie-card.scss';
 
 export class MovieCard extends React.Component {
   render() {
     const { movieData, onMovieClick } = this.props;
 
-    return <div className="movie-card" onClick={() => { onMovieClick(movieData); }}>
-        <h2>{movieData.Title}</h2>
-      </div>;
+    return (
+      
+    <Card className="card-view card text-center shadow m-1 mb-4" onClick={() => { onMovieClick(movieData)}}>
+      <div className="overflow">
+        <Card.Img className="card-img" variant="top" crossOrigin='anonymous' src={movieData.ImageUrl} />
+      </div>
+      <Card.Body className="card-body">
+        <Card.Title><h4>{movieData.Title}</h4></Card.Title>
+        <Card.Text>{movieData.Description}</Card.Text>
+      </Card.Body>
+    </Card>
+    )
   }
 }
 
