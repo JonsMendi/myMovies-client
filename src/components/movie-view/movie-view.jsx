8edb9f13bb6bcb8a2bf85e2ff871b7fab35f5f06
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-02-01 15:59:02 +0100

    Wrapped the rendered information into <Row> and <Col>.
    If applied, this commit will have use bootstrap styling to wrap all the rendered information into <Row> and <Col>.
    Was used as well the <Figure> to wrap the image.
    (This component still needs to be improved in styling)

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Row, Col, Button, Figure } from 'react-bootstrap';
 
 export class MovieView extends React.Component {
 
@@ -7,46 +8,56 @@ export class MovieView extends React.Component {
     const { movieData, onBackClick } = this.props;
     
     return (
-    <div className="movie-view">
-      <div className="movie-poster">
-        <img crossOrigin='anonymous' src={movieData.ImageUrl} />
-      </div>
-      <div className="movie-title">
-        <span className="label"></span>
-        <span className="value"><h2>{movieData.Title}</h2></span>
-      </div>
-      <div className="movie-description">
-        <span className="label">Description: </span>
-        <span className="value">{movieData.Description}</span>
-      </div>
-      <div className="movie-releaseYear">
-        <span className="label">Release Year: </span>
-        <span className="value">{movieData.ReleaseYear}</span>
-      </div>
-      <div className="movie-genre">
-        <span className="label">Genre: </span>
-        <span className="value">{movieData.Genre.Name}</span>
-      </div>
-      <div className="movie-director">
-        <span className="label">Director: </span>
-        <span className="value">{movieData.Director.Name}</span>
-      </div>
-      <div className="movie-actor">
-        <span className="label">Actors: </span>
-        <span className="value">{movieData.Actors}</span>
-      </div>
-      <div className="movie-rating">
-        <span className="label">Rating: </span>
-        <span className="value">{movieData.Rating}</span>
-      </div>
-      <div className="movie-featured">
-        <span className="label">Featured: </span>
-        <span className="value">{movieData.Featured}</span>
-      </div>
-      <div className="movie-button">
-        <button onClick={() => { onBackClick(null); }}>Get back!</button>
-      </div>
-    </div>
+      <Row className="justify-content-md-center text-center">
+        <Col>
+          <Figure>
+            <Figure.Image
+              width={270}
+              height={320}
+              alt="171x180"
+              crossOrigin='anonymous'
+              src={movieData.ImageUrl}
+            />
+          </Figure>
+          <div className="movie-view">
+            <div className="movie-title">
+              <span className="label"></span>
+              <span className="value"><h2>{movieData.Title}</h2></span>
+            </div>
+            <div className="movie-description">
+              <span className="label">Description: </span>
+              <span className="value">{movieData.Description}</span>
+            </div>
+            <div className="movie-releaseYear">
+              <span className="label">Release Year: </span>
+              <span className="value">{movieData.ReleaseYear}</span>
+            </div>
+            <div className="movie-genre">
+              <span className="label">Genre: </span>
+              <span className="value">{movieData.Genre.Name}</span>
+            </div>
+            <div className="movie-director">
+              <span className="label">Director: </span>
+              <span className="value">{movieData.Director.Name}</span>
+            </div>
+            <div className="movie-actor">
+              <span className="label">Actors: </span>
+              <span className="value">{movieData.Actors}</span>
+            </div>
+            <div className="movie-rating">
+              <span className="label">Rating: </span>
+              <span className="value">{movieData.Rating}</span>
+            </div>
+            <div className="movie-featured">
+              <span className="label">Featured: </span>
+              <span className="value">{movieData.Featured}</span>
+            </div>
+            <div className="m-4">
+              <Button onClick={() => { onBackClick(null); }}>Get back!</Button>
+            </div>
+          </div>
+        </Col>
+      </Row>
     );
   }
 }
