88bd75093fd7a273f9159d96ef592302181c5c5f
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-02-03 17:56:46 +0100

    Updated with new validate function creating the conditions in handleSubmit function.
    If applied, this commit will have been updated with new validate function creating the conditions in handleSubmit function.

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -2,16 +2,70 @@ import React, { useState } from 'react';
 import propTypes from 'prop-types';
 import './registration-view.scss';
 import { Container, Row, Col, Card,Form, Button } from 'react-bootstrap';
+import axios from 'axios';
 
 export function RegistrationView(props) {
     const [ username, setUsername ] = useState('');
     const [ password, setPassword ] = useState('');
     const [ email, setEmail ] = useState('');
     const [ birth, setBirth ] = useState('');
+    const [ usernameErr, setUsernameErr ] = useState('');
+    const [ passwordErr, setPasswordErr ] = useState('');
+    const [ emailErr, setEmailErr ] = useState('');
+    const [ birthErr, setBirthErr ] = useState('');
+
+    const validate = () => {
+        let isReq = true;
+        if(!username){
+            setUsernameErr('Username is required.');
+            isReq = false;
+        }else if(username.length < 5){
+            setUsernameErr('Username must be at least 5 characters long.');
+            isReq = false;
+        }
+        if(!password){
+            setPasswordErr('Password is required.');
+            isReq = false;
+        }else if(password.length < 5){
+            setPasswordErr('Password must be at least 5 characters long.');
+            isReq = false;
+        }
+        if(!email){
+            setEmailErr('Email is required.');
+            isReq = false;
+        }else if(email.indexOf('@') === -1){
+            setEmailErr('Email should contain "@".')
+            isReq = false;
+        }
+        if(!birth){
+            setBirthErr('Birthday is required.');
+            isReq = false;
+        }
+        return isReq;
+        
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        props.onRegister(username, password, email, birth)
+        const isReq = validate();
+        if(isReq){
+            axios.post('https://mymovies-api-jbm.herokuapp.com/users', {
+                Username: username,
+                Password: password,
+                Email: email,
+                Birth: birth
+            })
+            .then(response => {
+                const data = response.data;
+                console.log(data);
+                alert('You have been successfully registered, please log in.');
+                window.open('/', '_self');//'_self' to make sure that opens in the same tab instead of another one as is on default.
+            })
+            .catch(e => {
+                console.error(response)
+                alert('unable to register.');
+            }); 
+        }  
     };
 
     return (
@@ -24,19 +78,27 @@ export function RegistrationView(props) {
                             <Form>
                                 <Form.Group className="mb-4" controlId="formUsername">
                                     <Form.Label>Username</Form.Label>
-                                    <Form.Control type="text" placeholder="..." value={username} onChange={e => setUsername(e.target.value)}/>
+                                    <Form.Control type="text" placeholder="At least 5 characters" value={username} onChange={e => setUsername(e.target.value)}/>
+                                    {/* code added here to display validation error */}
+                                    {usernameErr && <p>{usernameErr}</p>}
                                 </Form.Group>
                                 <Form.Group className="mb-4" controlId="formPassword">
                                     <Form.Label>Password</Form.Label>
-                                    <Form.Control type="password" placeholder="..." value={password} onChange={e => setPassword(e.target.value)}/>
+                                    <Form.Control type="password" placeholder="At least 5 characters" value={password} onChange={e => setPassword(e.target.value)}/>
+                                    {/* code added here to display validation error */}
+                                    {passwordErr && <p>{passwordErr}</p>}
                                 </Form.Group>
                                 <Form.Group className="mb-4" controlId="formEmail">
                                     <Form.Label>Email Address</Form.Label>
-                                    <Form.Control type="email" placeholder="..." value={email} onChange={e => setEmail(e.target.value)}/>
+                                    <Form.Control type="email" placeholder="Do not forget to use '@'" value={email} onChange={e => setEmail(e.target.value)}/>
+                                    {/* code added here to display validation error */}
+                                    {emailErr && <p>{emailErr}</p>}
                                 </Form.Group>
                                 <Form.Group className="mb-4" controlId="formBirthday">
                                     <Form.Label>Birthday</Form.Label>
-                                    <Form.Control type="text" placeholder="..." value={birth} onChange={e => setBirth(e.target.value)}/>
+                                    <Form.Control type="text" placeholder="You dont need to be honest :)" value={birth} onChange={e => setBirth(e.target.value)}/>
+                                    {/* code added here to display validation error */}
+                                    {emailErr && <p>{emailErr}</p>}
                                 </Form.Group>
                                 <Button variant="secondary" type="submit" onClick={handleSubmit}>Register</Button>
                             </Form>
