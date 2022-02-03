8d42638b1f97e14a7a0974275acec751fe19abd6
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-02-03 18:08:15 +0100

    Updated onLoggedIn method, created getMovies method and created onLoggedOut method.
    If applied, this commit will have Updated onLoggedIn method, created getMovies method and created onLoggedOut method.
    In onLoggedIn method was defined a state and used the localStorage to keep the token and user to remember the browser that the user is still active and doesn't made him to log in again.
    The getMovies method was created to avoid repetition on the code since the '/movies' endpoint needed to be used in the moment that we Log and every time the user loads the page.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import axios from 'axios';
-import PropTypes from 'prop-types';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Row, Col, Button } from 'react-bootstrap';
 import { LoginView } from '../login-view/login-view';
 import { RegistrationView } from '../registration-view/registration-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
-import { ButtonLogout } from '../button/button-logout';
 import './main-view.scss';
 
 export class MainView extends React.Component {
@@ -18,29 +16,63 @@ export class MainView extends React.Component {
       super();
       this.state = {
         movies: [],
-        selectedMovie: null,
         user: null //initial state set to null
       };
     }
 
-    //sa
+    componentDidMount(){
+      // code executed right after the component is added to the DOM.
+      // is a good place to add code for performing async tasks such as making ajax requests or adding event listeners(ex: fetch the list of movies from your database).
+      // Under, every time a user loads the page and the componentDidMount method is called, you check if the user is logged in (by retrieving this information from localStorage). Only if the user is already logged in do you make the same GET request to the “movies” endpoint (by calling the getMovies method).
+      let accessToken = localStorage.getItem('token');
+      if (accessToken !== null) {
+        this.setState({
+          user: localStorage.getItem('user')
+        });
+        this.getMovies(accessToken);
+      }
+    }
+
+    ////Under the Methods that Updates the state.
     //When a movie is clicked this function is called and updated the state going to the selectedMovie.
     setSelectedMovie(newSelectedMovie) {
       this.setState({
         selectedMovie: newSelectedMovie
       });
     }
-    //If !user it activates this function. Goes to LoginView.
-    onLoggedIn(user) {
+    //If !user it activates this method. Goes to LoginView.
+    onLoggedIn(authData) {
+      console.log(authData);
       this.setState({
-        user
+        user: authData.user.Username
       });
+      //'localStorage'Tis a mechanism that stores the token and the logged in user in the browser, so that the next time you open your app, the browser remembers you’re already logged in. 
+      localStorage.setItem('token', authData.token);
+      localStorage.setItem('user', authData.user.Username);
+      this.getMovies(authData.token);//The moment a user logs in, a GET request(the method getMovies is defined under onLoggedIn method) is made to the “movies” endpoint by passing the bearer authorization in the header of the HTTP request (the getMovies method is called).
     }
     
-    //Register (not working, need to fix)
-    onRegister(registration) {
+    //Under, since a GET request has to be made to the “movies” endpoint in two cases (the moment a user logs in and every time a user loads(componentDidMount) the page), you refactored your code and moved the common code in the newly created getMovies(under) method to avoid repeating yourself.
+    getMovies(token) {
+      axios.get('https://mymovies-api-jbm.herokuapp.com/movies', {
+        headers: { Authorization: `Bearer ${token}`}
+      })
+      .then (response => {
+        //assign the result to set the state.
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    }
+
+    onLoggedOut() {
+      localStorage.removeItem('user');
+      localStorage.removeItem('token');
       this.setState({
-        registration
+        user: null
       });
     }
 
@@ -66,24 +98,13 @@ export class MainView extends React.Component {
           movies.map(movie => (
           <Col md={3}>
             <MovieCard key={movie._id} movieData={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
+            <Col>
+              <Button variant="secondary" onClick={() => { this.onLoggedOut() }} >Logout</Button>
+            </Col>
           </Col>
           ))
         }
         </Row>
       );
-    }
-
-    componentDidMount(){
-      // code executed right after the component is added to the DOM.
-      // is a good place to add code for performing async tasks such as making ajax requests or adding event listeners(ex: fetch the list of movies from your database).
-      axios.get('https://mymovies-api-jbm.herokuapp.com/movies')
-        .then(response => {
-          this.setState({
-            movies: response.data
-          });
-        })
-        .catch(error => {
-          console.log(error);
-        });
     }
 }
