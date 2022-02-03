5d2d0c783729b15a3644e346f4d269ea8a46cf90
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-02-03 18:09:26 +0100

    Updated with new validate function creating the conditions in handleSubmit to successfully loggin the user.
    If applied, this commit will have created a new validate function creating the conditions in handleSubmit to successfully loggin the user.

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import propTypes from 'prop-types';
 import { Row, Col, Card, Form, Button} from 'react-bootstrap';
 import './login-view.scss'
@@ -6,13 +7,46 @@ import './login-view.scss'
 export function LoginView(props) {
     const [ username, setUsername ] = useState('');
     const [ password, setPassword ] = useState('');
+    const [ usernameErr, setUsernameErr ] = useState('');
+    const [ passwordErr, setPasswordErr ] = useState('');
+
+    const validate = () => {
+        let isReq = true;
+        if(!username){
+            setUsernameErr('Username is required.');
+            isReq = false;
+        }else if(username.length < 5){
+            setUsernameErr('Username must be at least 5 characters long.');
+            isReq = false;
+        }
+        if(!password){
+            setPasswordErr('Password is required.')
+            isReq = false;
+        }else if(password.length < 5){
+            setPasswordErr('Password must be at least 5 characters long.');
+            isReq = false;
+        }
+        return isReq;
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(username, password);
-        /* Send a request to the server for authentication */
-        /* then call props.onLoggedIn(username) */
-        props.onLoggedIn(username);
+        const isReq = validate();
+            if(isReq) {
+            /* When a user clicks on the submit button of the login form, a POST request is made to the login endpoint of myMovies API using Axios. */
+            axios.post('https://mymovies-api-jbm.herokuapp.com/login', {
+                Username: username,
+                Password: password
+            })
+            .then(response => {
+                const data = response.data;
+                props.onLoggedIn(data);
+            })
+            .catch(e => {
+                console.log('User does not exist')
+                alert('User does not exist')
+            });
+        }
     };
 
     const handleRegistration = (e) => {
@@ -33,10 +67,14 @@ export function LoginView(props) {
                             <Form.Group className="mb-4" controlId="formUsername">
                                 <Form.Label>Username</Form.Label>
                                 <Form.Control type="text" placeholder="..." value={username} onChange={e => setUsername(e.target.value)}/>
+                                {/* code added here to display validation error */}
+                                    {usernameErr && <p>{usernameErr}</p>}
                             </Form.Group>
                             <Form.Group className="mb-4" controlId="formPassword">
                                 <Form.Label>Password</Form.Label>
                                 <Form.Control type="password" placeholder="..." value={password} onChange={e => setPassword(e.target.value)}/>
+                                 {/* code added here to display validation error */}
+                                    {passwordErr && <p>{passwordErr}</p>}
                             </Form.Group>
                             <Button variant="danger" type="submit" className="mr-2" onClick={handleSubmit}>Submit</Button>
                             <Button variant="secondary" className="mr-2" type="submit" onClick={handleRegistration}>Register</Button> 
