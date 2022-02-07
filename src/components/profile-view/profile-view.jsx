bde1cf47d5409c3573ffcd5dbeb0ef9a748e4964
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-02-07 20:51:46 +0100

    create new ProfileView class Component that allows the user to Update, Delete and add FavoriteMovies to the Profile.
    If applied, this commit will have create new ProfileView class Component that allows the user to Update, Delete and add FavoriteMovies to the Profile.
    In this Component is defined several functions to make actions available:
    - getUser(axios.get());
    - updateUser(axios.post());
    - removeFavoriteMovie(axios.delete());

---
+++ src/components/profile-view/profile-view.jsx
@@ -0,0 +1,203 @@
+import React from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import { Container, Row, Col, Card, Button, Form } from 'react-bootstrap';
+
+
+export class ProfileView extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            Username: null,
+            Password: null,
+            Email: null,
+            Birth: null,
+            FavoriteMovies: [],
+            
+        };
+    }
+
+    componentDidMount() {
+        const token = localStorage.getItem('token');
+        this.getUser(token);
+        console.log(this.props);
+    }
+
+    getUser = (token) => {
+        const Username = localStorage.getItem('user');
+        
+
+        axios.get(`https://mymovies-api-jbm.herokuapp.com/users/${Username}`, {
+            headers: { Authorization: `Bearer ${token}` },
+        })
+        .then(response => {
+            this.setState({
+                Username: response.data.Username,
+                Password: response.data.Password,
+                Email: response.data.Email,
+                Birth: response.data.Birth,
+                FavoriteMovies: response.data.FavoriteMovies
+            });
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
+
+    updateUser = (e) => {
+        e.preventDefault();
+        const Username = localStorage.getItem('user');
+        const token = localStorage.getItem('token');
+
+        axios.put(`https://mymovies-api-jbm.herokuapp.com/users/${Username}`, {
+            Username: this.state.Username,
+            Password: this.state.Password,
+            Email: this.state.Email,
+            Birth: this.state.Birth
+        }, { headers: { Authorization: `Bearer ${token}` }, })
+        .then((response) => {
+            this.setState({
+                Username: response.data.Username,
+                Password: response.data.Password,
+                Email: response.data.Email,
+                Birth: response.data.Birth
+            });
+            localStorage.setItem('user', response.data.Username)
+            alert('Profile was successfully updated');
+            window.location.pathname = "/";
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+        
+    }
+
+    deleteUser = (e) => {
+        const confirmDelete = window.confirm('Confirm to remove :(')
+
+        if (confirmDelete) {
+        const Username = localStorage.getItem('user');
+        const token = localStorage.getItem('token');
+        
+
+        axios.delete(`https://mymovies-api-jbm.herokuapp.com/users/${Username}`, 
+        {
+            headers: { Authorization: `Bearer ${token}` },
+        })
+        .then(() => {
+            localStorage.removeItem('user');
+            localStorage.removeItem('token');
+            alert('Profile successfully deleted')
+            window.location.pathname = "/";
+        })
+        .catch(function (error) {
+            console.log(error);
+        })};
+    }
+
+    removeFavoriteMovie = (e, movie) => {
+        e.preventDefault();
+        const Username = localStorage.getItem('user');
+        const token = localStorage.getItem('token');
+
+        axios.delete(`https://mymovies-api-jbm.herokuapp.com/users/${Username}/movies/${movie._id}`, 
+        { 
+            headers: { Authorization: `Bearer ${token}` } 
+        })
+        .then((response) => {
+            console.log(response);
+            alert('Movie was removed');
+            this.componentDidMount();
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
+
+    setUsername(value){
+        this.setState({
+            Username: value,
+        });
+    }
+
+    setPassword(value){
+        this.setState({
+            Password: value
+        });
+    }
+
+    setEmail(value){
+        this.setState({
+            Email: value
+        });
+    }
+
+    setBirth(value){
+        this.setState({
+            Birth: value
+        });
+    }
+
+    render () {
+        const { movie } = this.props;
+        const { FavoriteMovies, Username, Password, Email, Birth } = this.state;
+
+        return (
+            <Container className="profile-view" align="center">
+                <Row className="justify-content-md-center text-center">
+                    <Col md={8}>
+                        <Card border="danger" className="mt-5">
+                            <Card.Body>
+                                <Card.Title><h2>{Username} Room</h2></Card.Title>
+                                <Form className="form-update">
+                                    <Form.Group className="mb-4" controlId="formUsername">
+                                        <Form.Label>Username</Form.Label>
+                                        <Form.Control type="text" name="Username" placeholder="New Username" value={Username} onChange={e => this.setUsername(e.target.value)} required/>       
+                                    </Form.Group>
+                                    <Form.Group className="mb-4" controlId="formPassword">
+                                        <Form.Label>Password</Form.Label>
+                                        <Form.Control type="password" name="Password" placeholder="New Password" value={Password} onChange={e => this.setPassword(e.target.value)} required/>
+                                    </Form.Group>
+                                    <Form.Group className="mb-4" controlId="formEmail">
+                                        <Form.Label>Email</Form.Label>
+                                        <Form.Control type="email" name="Email" placeholder="New Email" value={Email} onChange={e => this.setEmail(e.target.value)} required/>
+                                    </Form.Group>
+                                    <Form.Group className="mb-4" controlId="formBirth">
+                                        <Form.Label>Birthday</Form.Label>
+                                        <Form.Control type="date" name="Birth" value={Birth} onChange={e => this.setBirth(e.target.value)}/>
+                                    </Form.Group>
+                                    <Button variant="danger" type="submit" className="mr-2" onClick={this.updateUser}>Update Profile</Button>
+                                    <Button variant="danger" type="submit" className="mr-2" onClick={this.deleteUser}>Delete Profile</Button>
+                                </Form>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row className="mt-5">
+                    <Col>
+                        <h4>My Favorite Movies</h4>
+                    </Col>
+                </Row>
+                <Row className="favorite-movies">
+                    {/*Under created a logic to generate the movies*/}
+                    { movie.map((movie) => {
+                        if(movie._id === FavoriteMovies.find((fv) => fv === movie._id))
+                        { return (
+                            <Col>
+                            <Card className="m-2" key={movie._id}  xs={12} md={6} lg={3}>
+                                <Card.Img className="fav-poster" variant="top" crossOrigin='anonymous' src={movie.ImageUrl}/>
+                                <Card.Body>
+                                    <Card.Title className="movie_title">{movie.Title}</Card.Title>
+                                    <Button variant="outline-primary" value={movie._id} onClick={(e) => { this.removeFavoriteMovie(e, movie)}}>Remove</Button>
+                                </Card.Body>
+                            </Card> 
+                            </Col>
+                        )}}) }
+               </Row>
+            </Container>
+        );
+
+    }
+
+
+}
