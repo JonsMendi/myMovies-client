a99451ebf6e4ca786ccc43190c7003599e815d72
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-02-07 20:37:26 +0100

    added new "react-router-dom" that allows to connect different endpoints through <Link>
    If applied, this commit will have imported the "react-router-dom" to create routes connecting compnents.

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -4,31 +4,36 @@ import  Button  from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import './movie-card.scss';
 
+import { Link } from "react-router-dom";
+
 export class MovieCard extends React.Component {
   render() {
-    const { movieData, onMovieClick } = this.props;
-
+    const { movie } = this.props;
+  console.log(movie);
     return (
       
-    <Card className="card-view card text-center shadow m-1 mb-4" onClick={() => { onMovieClick(movieData)}}>
-      <div className="overflow">
-        <Card.Img className="card-img" variant="top" crossOrigin='anonymous' src={movieData.ImageUrl} />
-      </div>
-      <Card.Body className="card-body">
-        <Card.Title><h4>{movieData.Title}</h4></Card.Title>
-        <Card.Text>{movieData.Description}</Card.Text>
-      </Card.Body>
-    </Card>
+      <Card className="card-view card text-center shadow m-1 mb-4">
+        <Link className="card-click" to={`/movies/${movie._id}`}>
+          <Button variant="none"> 
+          <div className="overflow">
+            <Card.Img className="card-img" variant="top" crossOrigin='anonymous' src={movie.ImageUrl} />
+          </div>
+        </Button>
+        </Link>
+        <Card.Body className="card-body">
+          <Card.Title><h4>{movie.Title}</h4></Card.Title>
+          <Card.Text>{movie.Description}</Card.Text>
+        </Card.Body>
+      </Card>
     )
   }
 }
 
 // propTypes - Give warnings in browser/console if data does not match with the required.
 MovieCard.propTypes = {
-  movieData: PropTypes.shape({
+  movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
     ImageUrl: PropTypes.string.isRequired
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
 };
