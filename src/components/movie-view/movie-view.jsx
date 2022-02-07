3efb4d4423b4b4a3e359444ba2a5cbcae0f6c810
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-02-07 20:28:34 +0100

    added a new 'addFavoriteMovie' function that allows the user to add a movie to is Favorite List
    If applied, this commit will have added a new function 'addFavoriteMovie' executed in a Button to allow the user to add a respective movie/movies to is own Favorite list of movies in the ProfileView.

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import axios from 'axios';
 import PropTypes from 'prop-types';
-import { Row, Col, Button, Figure } from 'react-bootstrap';
+import { Row, Col, Button, Figure, Link } from 'react-bootstrap';
+import { Link } from "react-router-dom";
 
 export class MovieView extends React.Component {
 
+  
+
+  addFavoriteMovie = (e, movie) => {
+    e.preventDefault();
+    const Username = localStorage.getItem('user');
+    const token = localStorage.getItem('token');
+    console.log(token);
+    axios.post(`https://mymovies-api-jbm.herokuapp.com/users/${Username}/movies/${this.props.movie._id}`, {},
+    { 
+      headers: { Authorization: `Bearer ${token}` },
+    })
+    .then((response) => {
+      console.log(response);
+            alert('Movie was added to Favorites');
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
   render() {
-    const { movieData, onBackClick } = this.props;
+    const { movie, actor } = this.props;
     
     return (
       <Row className="justify-content-md-center text-center">
@@ -16,44 +38,45 @@ export class MovieView extends React.Component {
               height={320}
               alt="171x180"
               crossOrigin='anonymous'
-              src={movieData.ImageUrl}
+              src={movie.ImageUrl}
             />
           </Figure>
           <div className="movie-view">
             <div className="movie-title">
               <span className="label"></span>
-              <span className="value"><h2>{movieData.Title}</h2></span>
+              <span className="value"><h2>{movie.Title}</h2></span>
             </div>
             <div className="movie-description">
               <span className="label">Description: </span>
-              <span className="value">{movieData.Description}</span>
+              <span className="value">{movie.Description}</span>
             </div>
             <div className="movie-releaseYear">
               <span className="label">Release Year: </span>
-              <span className="value">{movieData.ReleaseYear}</span>
+              <span className="value">{movie.ReleaseYear}</span>
             </div>
             <div className="movie-genre">
               <span className="label">Genre: </span>
-              <span className="value">{movieData.Genre.Name}</span>
+              <Link to={`/genres/${movie.Genre.Name}`}><Button variant="link">Genre</Button></Link>
             </div>
             <div className="movie-director">
               <span className="label">Director: </span>
-              <span className="value">{movieData.Director.Name}</span>
+              <Link to={`/directors/${movie.Director.Name}`}><Button variant="link">Director</Button></Link>
             </div>
             <div className="movie-actor">
               <span className="label">Actors: </span>
-              <span className="value">{movieData.Actors}</span>
+              <Link to={`/actors/${actor}`}><Button variant="link">Actor</Button></Link>
             </div>
             <div className="movie-rating">
               <span className="label">Rating: </span>
-              <span className="value">{movieData.Rating}</span>
+              <span className="value">{movie.Rating}/10</span>
             </div>
             <div className="movie-featured">
               <span className="label">Featured: </span>
-              <span className="value">{movieData.Featured}</span>
+              <span className="value">{movie.Featured}</span>
             </div>
             <div className="m-4">
-              <Button onClick={() => { onBackClick(null); }}>Get back!</Button>
+              <Link to={`/`}><Button variant='dark'>Back</Button></Link>
+              <Button className="ml-3" variant="primary" value={movie._id} onClick={(e) => this.addFavoriteMovie(e, movie)}>Add to Favorites</Button>
             </div>
           </div>
         </Col>
@@ -64,7 +87,7 @@ export class MovieView extends React.Component {
 
 // propTypes - Give warnings in browser/console if data does not match with the required.
 MovieView.propTypes = {
-  movieData: PropTypes.shape({
+  movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
     ImageUrl: PropTypes.string.isRequired,
