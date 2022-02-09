246e4f0ef0102f5980867b0388b5a1c544cc7cb1
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-02-09 10:22:06 +0100

    Changed from class Component to function Component.
    If applied, this commit will have changed from class Component to function Component.

--- src/components/actor-view/actor-view.jsx
+++ src/components/actor-view/actor-view.jsx
@@ -3,43 +3,38 @@ import { Link } from 'react-router-dom';
 import { Button, Col, Row, Card } from 'react-bootstrap';
 import './actor-view.scss';
 
-export class ActorView extends React.Component {
-    render () {
-        const { actor } = this.props;
-        
-    
+export function ActorView (props) {
+    const { actor } = props;
 
-        return (
-            <Row>
-                <Col>
-                    <Card>
-                        <Card.Body>
-                            <div>
-                                <Card.Title><h2></h2></Card.Title>
-                                <div className="director-bio">
-                                <span className="label">Bio: </span>
-                                <span className="value"><p>{actor}</p></span>
-                                </div>
-                                <div className="director-movies">
-                                <span className="label">Movies: </span>
-                                <span className="value"><p></p></span>
-                                </div>
-                                <div className="director-birth">
-                                <span className="label">Birth: </span>
-                                <span className="value"><p></p></span>
-                                </div>
-                                <div className="director-death">
-                                <span className="label">Death: </span>
-                                <span className="value"><p></p></span>
-                                </div>
-                                <Link to={`/`}><Button variant='dark'>Back</Button></Link>
+    return (
+        <Row>
+            <Col>
+                <Card>
+                    <Card.Body>
+                        <div>
+                            <Card.Title><h2>{actor}</h2></Card.Title>
+                            <div className="director-bio">
+                            <span className="label">Bio: </span>
+                            <span className="value"><p></p></span>
                             </div>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-        ) 
-    }
-    
+                            <div className="director-movies">
+                            <span className="label">Movies: </span>
+                            <span className="value"><p></p></span>
+                            </div>
+                            <div className="director-birth">
+                            <span className="label">Birth: </span>
+                            <span className="value"><p></p></span>
+                            </div>
+                            <div className="director-death">
+                            <span className="label">Death: </span>
+                            <span className="value"><p></p></span>
+                            </div>
+                            <Link to={`/`}><Button variant='dark'>Back</Button></Link>
+                        </div>
+                    </Card.Body>
+                </Card>
+            </Col>
+        </Row>
+    )
 
 }
