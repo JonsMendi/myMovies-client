a4897b8b74274a60cbaa9063aa1e68d3bf258611
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-06-27 16:25:15 +0200

    some bugs fixed.
    if applied this commit will have fixed some bugs.

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,31 +1,38 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import  Button  from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import './movie-card.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import "./movie-card.scss";
 
 import { Link } from "react-router-dom";
 
 export class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-  console.log(movie);
+    console.log(movie);
     return (
-      
       <Link className="card-click" to={`/movies/${movie._id}`}>
-          <Button variant="none"> 
-      <Card className="movie-card-view card text-center shadow m-1 mb-4 overflow ">
-          <div className="card-img">
-            <Card.Img variant="top" crossOrigin='anonymous' src={movie.ImageUrl} />
-          </div>
-        <Card.Body className="card-body">
-          <Card.Title><h4>{movie.Title}</h4></Card.Title>
-          <Card.Text>{movie.Description}</Card.Text>
-        </Card.Body>
-      </Card>
-       </Button>
-        </Link>
-    )
+        <Button variant="none">
+          <Card className="movie-card-view card text-center shadow m-1 mb-4 overflow ">
+            <div className="card-img">
+              <Card.Img
+                variant="top"
+                crossOrigin="anonymous"
+                src={movie.ImageUrl}
+              />
+            </div>
+            <Card.Body className="card-body">
+              <Card.Title>
+                <h4>{movie.Title}</h4>
+              </Card.Title>
+              <Card.Text maxLength={5}>
+                {movie.Description.substr(0, 100)}...
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Button>
+      </Link>
+    );
   }
 }
 
@@ -34,6 +41,6 @@ MovieCard.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
-    ImageUrl: PropTypes.string.isRequired
-  })
+    ImageUrl: PropTypes.string.isRequired,
+  }),
 };
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,61 +1,64 @@
-import React from 'react';
-import axios from 'axios';
-import PropTypes from 'prop-types';
-import { Row, Col, Button, Figure, Card } from 'react-bootstrap';
+import React from "react";
+import axios from "axios";
+import PropTypes from "prop-types";
+import { Row, Col, Button, Figure, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import './movie-view.scss';
+import "./movie-view.scss";
 
 export class MovieView extends React.Component {
-
   addFavoriteMovie = (e) => {
     e.preventDefault();
-    const Username = localStorage.getItem('user');
-    const token = localStorage.getItem('token');
+    const Username = localStorage.getItem("user");
+    const token = localStorage.getItem("token");
     console.log(token);
-    axios.post(`https://mymovies-api-jbm.herokuapp.com/users/${Username}/movies/${this.props.movie._id}`, {},
-    { 
-      headers: { Authorization: `Bearer ${token}` },
-    })
-    .then((response) => {
-      console.log(response);
-      alert('Movie was added to Favorites');
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  }
+    axios
+      .post(
+        `https://mymovies-api-jbm.herokuapp.com/users/${Username}/movies/${this.props.movie._id}`,
+        {},
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      )
+      .then((response) => {
+        console.log(response);
+        alert("Movie was added to Favorites");
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
   removeFavoriteMovie = (e, movie) => {
     e.preventDefault();
-    const Username = localStorage.getItem('user');
-    const token = localStorage.getItem('token');
+    const Username = localStorage.getItem("user");
+    const token = localStorage.getItem("token");
 
-    axios.delete(`https://mymovies-api-jbm.herokuapp.com/users/${Username}/movies/${movie._id}`, 
-    { 
-        headers: { Authorization: `Bearer ${token}` } 
-    })
-    .then((response) => {
+    axios
+      .delete(
+        `https://mymovies-api-jbm.herokuapp.com/users/${Username}/movies/${movie._id}`,
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      )
+      .then((response) => {
         console.log(response);
-        alert('Movie was removed');
+        alert("Movie was removed");
         this.componentDidMount();
-    })
-    .catch(function (error) {
+      })
+      .catch(function (error) {
         console.log(error);
-    });
-}
+      });
+  };
 
   render() {
     const { movie, actor } = this.props;
 
-    
     return (
-      
-      <div className="mt-4 mx-auto" style={{ width: "60%" }}>
-         <Card className="d-flex flex-column flex-md-row align-items-center ml-xs-5 border-def">
-          <Card.Body className='mr-5 pr-5'>
-          <span className="d-flex align-items-center">
-              
-              <h1 className="display-4">{movie.Title}</h1>
+      <div className="mt-4 mx-auto" style={{ width: "100%" }}>
+        <Card className="d-flex flex-column flex-md-row align-items-center ml-xs-5 border-def">
+          <Card.Body className="mr-5 pr-5">
+            <span className="d-flex align-items-center">
+              <h1 className="display-5">{movie.Title}</h1>
             </span>
             <span className="d-flex align-items-center">
               <h5 className="text-muted mt-4 mr-3">Genre: </h5>
@@ -71,25 +74,43 @@ export class MovieView extends React.Component {
             </span>
             <span className="d-flex align-items-center mb-2">
               <h5 className="text-muted mr-2">Release Year: </h5>
-                <h6 className="">{movie.ReleaseYear}</h6>  
+              <h6 className="">{movie.ReleaseYear}</h6>
             </span>
             <span className="d-flex align-items-center mb-2">
               <h5 className="text-muted mr-2">Rating: </h5>
-                <h6 className="">{movie.Rating}/10</h6>
+              <h6 className="">{movie.Rating}/10</h6>
             </span>
-              <p>{movie.Description}</p>
+            <p>{movie.Description}</p>
           </Card.Body>
-          <img width={270} height={380} variant="right" className="ml-3" crossOrigin='anonymous' src={movie.ImageUrl}  />
-          </Card>
-          {/*<Card.Text>Actor: <Link to={`/actors/${actor}`}><Button variant="link">{movie.Actor}</Button></Link></Card.Text>*/}
-          
-            <Link to={`/`}><Button className="back-btn mt-4 mr-3 ml-3" variant='outline-secondary'>Back</Button></Link>
-            <Button className="add-favorite-btn mt-4" variant="outline-light" value={movie._id} onClick={(e) => this.addFavoriteMovie(e, movie)}>
-            <span className="d-flex align-items-center">
+          <img
+            width={270}
+            height={380}
+            variant="right"
+            className="ml-3"
+            crossOrigin="anonymous"
+            src={movie.ImageUrl}
+          />
+        </Card>
+        {/*<Card.Text>Actor: <Link to={`/actors/${actor}`}><Button variant="link">{movie.Actor}</Button></Link></Card.Text>*/}
+
+        <Link to={`/`}>
+          <Button
+            className="back-btn mt-4 mr-3 ml-3"
+            variant="outline-secondary"
+          >
+            Back
+          </Button>
+        </Link>
+        <Button
+          className="add-favorite-btn mt-4"
+          variant="outline-light"
+          value={movie._id}
+          onClick={(e) => this.addFavoriteMovie(e, movie)}
+        >
+          <span className="d-flex align-items-center">
             <i className="material-icons heart mr-3">Add to favorites</i>
-              </span>
-            </Button>
-          
+          </span>
+        </Button>
       </div>
     );
   }
@@ -104,17 +125,17 @@ MovieView.propTypes = {
     ReleaseYear: PropTypes.string.isRequired,
     Genre: PropTypes.shape({
       Name: PropTypes.string.isRequired,
-      Description: PropTypes.string.isRequired
+      Description: PropTypes.string.isRequired,
     }),
     Director: PropTypes.shape({
       Name: PropTypes.string.isRequired,
       Bio: PropTypes.string.isRequired,
       Birth: PropTypes.string.isRequired,
-      Death: PropTypes.string.isRequired
+      Death: PropTypes.string.isRequired,
     }),
     Actors: PropTypes.array.isRequired,
     Rating: PropTypes.string.isRequired,
-    Featured: PropTypes.bool.isRequired
+    Featured: PropTypes.bool.isRequired,
   }).isRequired,
-  onBackClick: PropTypes.func.isRequired
+  onBackClick: PropTypes.func.isRequired,
 };
